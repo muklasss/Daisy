96599415551d433e98ff6c188242d73361050379
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-11-06 08:27:51 +0530

    Update filters.py

--- emilia/modules/helper_funcs/filters.py
+++ emilia/modules/helper_funcs/filters.py
@@ -9,19 +9,13 @@ class CustomFilters(object):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
 
-    support_filter = '1141839926 1186105905 694380168'
+    support_filter = _Supporters()
 
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
 
-    sudo_filter = '1141839926 1186105905 694380168'
-
-    class _Developers(BaseFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUDO_USERS)
-
-    dev_filter = '1141839926 1186105905 694380168'
+    sudo_filter = _Sudoers()
 
     class _MimeType(BaseFilter):
         def __init__(self, mimetype):
@@ -38,3 +32,4 @@ class CustomFilters(object):
             return bool(message.text or message.sticker or message.photo or message.document or message.video)
 
     has_text = _HasText()
+
