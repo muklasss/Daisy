1da1b667b5d8532b3b2621f6ea31846cceb36988
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-01 23:34:06 +0530

    Update wiki.py

--- emilia/modules/wiki.py
+++ emilia/modules/wiki.py
@@ -17,14 +17,18 @@ from emilia.__main__ import STATS, USER_INFO
 from emilia.modules.disable import DisableAbleCommandHandler
 import wikipedia
 
-def wiki(bot: Bot, update: Update, args):
+def wiki(update, context):
+    args=context.args
     reply = " ".join(args)
-    summary = '{} <a href="{}">more</a>'
+    summary = 'define {} <a href="{}">more</a>'
     update.message.reply_text(summary.format(wikipedia.summary(reply, sentences=3), wikipedia.page(reply).url))
 		
 __help__ = """
- - /wiki text: Returns search from wikipedia for the input text
+ Ever stumbled upon a word that you didn't know of and wanted to look it up?
+With this module, you can find the definitions of words without having to leave the app!
+*Available commands:*
+ - /define <word>: returns the definition of the word.
 """
-__mod_name__ = "WikiPedia"
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
+__mod_name__ = "DICTIONARY"
+WIKI_HANDLER = DisableAbleCommandHandler("define", wiki, pass_args=True)
 dispatcher.add_handler(WIKI_HANDLER)
