89ed6a59ef2770c0d05687ac8bb4209a6f8d0a82
Inuka ASiTH <70461117+InukaAsith@users.noreply.github.com>
2020-12-03 18:29:37 +0530

    Update autoreply.py

--- emilia/modules/autoreply.py
+++ emilia/modules/autoreply.py
@@ -21,25 +21,28 @@ A=False
 B=False
 C=False
 D=False
-# content of the automatic reply
-if __name__ == '__main__':
+client = TelegramClient(session_file, api_id, api_hash, sequential_updates=True)
+
+
+@client.on(events.NewMessage(incoming=True))
+async def handle_new_message(event):
+    
+        
+    if event.is_private:  # only auto-reply to private chats
+        
+            
+       from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
+       if not from_.bot:  # don't auto-reply to bots
+            
+                
+            print(time.asctime(), '-', event.message)  # optionally log time and message
+            time.sleep(1)  # pause for 1 second to rate-limit automatic replies
+            await event.respond(message)
+
     
                 # Create the client and connect
                 # use sequential_updates=True to respond to messages one at a time
-    client = TelegramClient(session_file, api_id, api_hash, sequential_updates=True)
 
-
-    @client.on(events.NewMessage(incoming=True))
-    async def handle_new_message(event):
-        
-        if event.is_private:  # only auto-reply to private chats
-            
-            from_ = await event.client.get_entity(event.from_id)  # this lookup will be cached by telethon
-            if not from_.bot:  # don't auto-reply to bots
-                
-                print(time.asctime(), '-', event.message)  # optionally log time and message
-                time.sleep(1)  # pause for 1 second to rate-limit automatic replies
-                await event.respond(message)
 
 
                 
