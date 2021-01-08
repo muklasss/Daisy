e8adfaf0d3a0d1a833b46811f20ee1be1aa21aac
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-08 15:25:48 +0530

    Update songsy.py

--- emilia/modules/songsy.py
+++ emilia/modules/songsy.py
@@ -278,8 +278,7 @@ async def download_video(v_url):
 
 
 __help__ = """
- ➩ /song <songname artist(optional)>: uploads the song in it's best quality available
- ➩ /video <songname artist(optional)>: uploads the video song in it's best quality available
+ ➩ Coming Soon
 """
 
 __mod_name__ = "SONGS"
