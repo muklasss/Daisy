30313570cde019c255f19a88569f9974200ae71d
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-08 14:54:57 +0530

    Update texttospeech.py

--- emilia/modules/texttospeech.py
+++ emilia/modules/texttospeech.py
@@ -1,84 +1,39 @@
-
-from emilia import client
-import os
-
+from telegram import ChatAction
 from gtts import gTTS
-from gtts import gTTSError
-from telethon import *
-from telethon.tl import functions
-from telethon.tl import types
-from telethon.tl.types import *
+import html
+import urllib.request
+import re
+import json
+from datetime import datetime
+from typing import Optional, List
+import time
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from telegram.utils.helpers import escape_markdown, mention_html
+from emilia import dispatcher
+from emilia.__main__ import STATS
+from emilia.modules.disable import DisableAbleCommandHandler
+from emilia.modules.helper_funcs.extraction import extract_user
 
-from emilia import *
-
-from emilia.events import register
-
-
-async def is_register_admin(chat, user):
-    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
-
-        return isinstance(
-            (await
-             client(functions.channels.GetParticipantRequest(chat,
-                                                           user))).participant,
-            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
-        )
-    if isinstance(chat, types.InputPeerChat):
-
-        ui = await client.get_peer_id(user)
-        ps = (await client(functions.messages.GetFullChatRequest(chat.chat_id)
-                         )).full_chat.participants.participants
-        return isinstance(
-            next((p for p in ps if p.user_id == ui), None),
-            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
-        )
-    return None
-
-
-@register(pattern="^/tts (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    if event.is_group:
-     if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-       await event.reply("😜 Hai.. You are not admin..🤭 You can't use this command.. But you can use in my pm🙈")
-       return
-
-    input_str = event.pattern_match.group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        text = previous_message.message
-        lan = input_str
-    elif "|" in input_str:
-        lan, text = input_str.split("|")
-    else:
-        await event.reply(
-            "Invalid Syntax\nFormat `/tts lang | text`\nFor eg: `/tts en | hello`"
-        )
-        return
-    text = text.strip()
-    lan = lan.strip()
-    try:
-        tts = gTTS(text, tld="com", lang=lan)
+def tts(bot: Bot, update: Update, args):
+    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
+    reply = " ".join(args)
+    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+    lang="ml"
+    tts = gTTS(reply, lang)
+    tts.save("k.mp3")
+    with open("k.mp3", "rb") as f:
+        linelist = list(f)
+        linecount = len(linelist)
+    if linecount == 1:
+        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+        lang = "en"
+        tts = gTTS(reply, lang)
         tts.save("k.mp3")
-    except AssertionError:
-        await event.reply("The text is empty.\n"
-                          "Nothing left to speak after pre-precessing, "
-                          "tokenizing and cleaning.")
-        return
-    except ValueError:
-        await event.reply("Language is not supported.")
-        return
-    except RuntimeError:
-        await event.reply("Error loading the languages dictionary.")
-        return
-    except gTTSError:
-        await event.reply("Error in Google Text-to-Speech API request !")
-        return
-    with open("k.mp3", "r"):
-        await client.send_file(event.chat_id,
-                             "k.mp3",
-                             voice_note=True,
-                             reply_to=reply_to_id)
-        os.remove("k.mp3")
+    with open("k.mp3", "rb") as speech:
+        update.message.reply_voice(speech, quote=False)
+
+dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
