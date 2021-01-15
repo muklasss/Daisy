bbff57febed721c2eedbc71c116a55e5e04ef3fd
Inuka ASiTH <inukaasith7@gmail.com>
2021-01-15 15:17:08 +0530

    Update texttospeech.py

--- emilia/modules/texttospeech.py
+++ emilia/modules/texttospeech.py
@@ -1,23 +1,33 @@
+import html
+import re
+from datetime import datetime
+from typing import List
+import random
 from telegram import ChatAction
 from gtts import gTTS
-import html
-import urllib.request
-import re
-import json
-from datetime import datetime
-from typing import Optional, List
 import time
+from telegram import ChatAction
+from feedparser import parse
+import json
+import urllib.request
+import urllib.parse
 import requests
-from telegram import Message, Chat, Update, Bot, MessageEntity
-from telegram import ParseMode
-from telegram.ext import CommandHandler, run_async, Filters
-from telegram.utils.helpers import escape_markdown, mention_html
-from emilia import dispatcher
-from emilia.__main__ import STATS
-from emilia.modules.disable import DisableAbleCommandHandler
-from emilia.modules.helper_funcs.extraction import extract_user
 
-def tts(bot: Bot, update: Update, args):
+from emilia import (DEV_USERS, OWNER_ID, DRAGONS, DEMONS,
+                          TIGERS, WOLVES, dispatcher, updater)
+from emilia.__main__ import STATS, TOKEN, USER_INFO
+from emilia.modules.disable import DisableAbleCommandHandler
+from emilia.modules.helper_funcs.filters import CustomFilters
+from emilia.modules.helper_funcs.chat_status import sudo_plus, user_admin
+from telegram import MessageEntity, ParseMode, Update, constants
+from telegram.error import BadRequest
+from emoji import UNICODE_EMOJI
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.utils.helpers import mention_html
+
+@run_async
+def tts(update: Update, context: CallbackContext):
+    args = context.args
     current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     filename = datetime.now().strftime("%d%m%y-%H%M%S%f")
     reply = " ".join(args)
@@ -36,4 +46,13 @@ def tts(bot: Bot, update: Update, args):
     with open("k.mp3", "rb") as speech:
         update.message.reply_voice(speech, quote=False)
 
-dispatcher.add_handler(CommandHandler('tts', tts, pass_args=True))
+__help__ = """
+ • `/tts <text>`*:* convert text to speech
+ • `/stt <text>`*:* convert speech to text
+ """
+TTS_HANDLER = DisableAbleCommandHandler("tts", tts, pass_args=True)
+dispatcher.add_handler(TTS_HANDLER)
+
+__mod_name__ = "TTS - STT"
+__command_list__ = ["tts"]
+__handlers__ = [TTS_HANDLER]
